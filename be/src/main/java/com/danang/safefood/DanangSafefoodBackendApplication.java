7ea804fc01fc952216fd429b6b0c22c3395409f3
NguyenTuan180505 <hung0935347609@gmail.com>
2026-04-23 15:42:10 +0700

    add backend source code

---
+++ be/src/main/java/com/danang/safefood/DanangSafefoodBackendApplication.java
@@ -0,0 +1,13 @@
+package com.danang.safefood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DanangSafefoodBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DanangSafefoodBackendApplication.class, args);
+	}
+
+}
