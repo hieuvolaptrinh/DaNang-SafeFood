fe34f707076c00cf59d030d8508fb4d97346c96d
NguyenTuan180505 <hung0935347609@gmail.com>
2026-05-08 21:03:13 +0700

    Tuấn thêm các chức năng của Kiểm định viên

---
+++ be/src/main/java/com/danang/safefood/dto/response/ViPhamResponse.java
@@ -0,0 +1,27 @@
+package com.danang.safefood.dto.response;
+
+import com.danang.safefood.entity.ViPham;
+
+public record ViPhamResponse(
+        String maViPham,
+        String moTaThem,
+        String khacPhuc,
+        String trangThaiPheDuyet,
+        String mucDo,
+        String maHoSo,
+        String maLoaiViPham,
+        String tenLoaiViPham
+) {
+    public static ViPhamResponse from(ViPham e) {
+        return new ViPhamResponse(
+                e.getMaViPham(),
+                e.getMoTaThem(),
+                e.getKhacPhuc(),
+                e.getTrangThaiPheDuyet(),
+                e.getMucDo(),
+                e.getHoSoThanhTra() != null ? e.getHoSoThanhTra().getMaHoSo() : null,
+                e.getLoaiViPham() != null ? e.getLoaiViPham().getMaLoaiViPham() : null,
+                e.getLoaiViPham() != null ? e.getLoaiViPham().getTenLoaiViPham() : null
+        );
+    }
+}
